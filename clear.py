d5947754a540802fbe6ececfdee0a625decee097
tong-xz <txz2001@163.com>
2023-10-31 18:27:08 -0400

    update

--- clear.py
+++ clear.py
@@ -1,2 +1,7 @@
 import torch
+#https://stackoverflow.com/questions/59129812/how-to-avoid-cuda-out-of-memory-in-pytorch
 torch.cuda.empty_cache()
+torch.cuda.memory_summary(device=None, abbreviated=False)
+
+import gc
+gc.collect()
